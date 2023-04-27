f042c20e417fe095208a2fa37eec2d43e1e2de2b
Jenniffer Granados <gm14032@ues.edu.sv>
2023-04-27 01:49:11 +0100

    :zap: register a new user and validate two urls

--- src/main/java/com/restaurante/dsi/controller/UserRestController.java
+++ src/main/java/com/restaurante/dsi/controller/UserRestController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,15 +20,28 @@ public class UserRestController {
 
   @Autowired
   private IUserService userService;
-  
-  @GetMapping({"/", ""})
+
+  @GetMapping({ "/", "" })
   public List<Users> index() {
     return userService.findAll();
   }
 
+  @PostMapping("/register")
+  public Users register(@RequestBody Users user) {
+    BCryptPasswordEncoder pass = new BCryptPasswordEncoder();
+    user.setPassword(pass.encode(user.getPassword()));
+    return userService.save(user);
+  }
+
   @GetMapping("/auth")
-  @PreAuthorize("hasPrivilege('ADMIN')")
+  @PreAuthorize("isAuthenticated()")
   public List<Users> authorize() {
+    return userService.findAll();
+  }
+
+  @GetMapping("/role")
+  @PreAuthorize("hasPermission(#user, 'READ_USER')")
+  public List<Users> testAdmin() {
     return userService.findAll();
   }
 }
