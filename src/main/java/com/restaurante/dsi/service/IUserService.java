2c850b1db7095e76b239e6a35363667358db4917
Jenniffer Granados <gm14032@ues.edu.sv>
2023-04-27 01:45:06 +0100

    :zap: load user from database

--- src/main/java/com/restaurante/dsi/service/IUserService.java
+++ src/main/java/com/restaurante/dsi/service/IUserService.java
@@ -2,8 +2,14 @@ package com.restaurante.dsi.service;
 
 import java.util.List;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.restaurante.dsi.model.Users;
 
 public interface IUserService {
   public List<Users> findAll();
+
+  public UserDetails loadUserByUsername(String username);
+
+  public Users save(Users user);
 }
--- src/main/java/com/restaurante/dsi/service/iml/UserServiceImpl.java
+++ src/main/java/com/restaurante/dsi/service/iml/UserServiceImpl.java
@@ -3,15 +3,18 @@ package com.restaurante.dsi.service.iml;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import com.restaurante.dsi.model.Users;
 import com.restaurante.dsi.repository.IUsers;
 import com.restaurante.dsi.service.IUserService;
+import com.restaurante.dsi.utils.UserDetailsImpl;
 
 @Service("UserService")
-public class UserServiceImpl implements IUserService {
-  
+public class UserServiceImpl implements IUserService, UserDetailsService {
+
   @Autowired
   private IUsers usersRepository;
 
@@ -19,5 +22,17 @@ public class UserServiceImpl implements IUserService {
   public List<Users> findAll() {
     return usersRepository.findAll();
   }
-  
+
+  @Override
+  public UserDetails loadUserByUsername(String username) {
+    Users user = usersRepository.findByUsername(username);
+
+    return UserDetailsImpl.build(user);
+  }
+
+  @Override
+  public Users save(Users user) {
+    return usersRepository.save(user);
+  }
+
 }
