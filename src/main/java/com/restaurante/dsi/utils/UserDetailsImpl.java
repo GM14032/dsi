cfc05163a553311ed7e61671a59177aab4d8daf2
Jenniffer Granados <gm14032@ues.edu.sv>
2023-04-27 01:45:57 +0100

    :zap: add implementation of users details

---
+++ src/main/java/com/restaurante/dsi/utils/UserDetailsImpl.java
@@ -0,0 +1,116 @@
+package com.restaurante.dsi.utils;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.restaurante.dsi.model.Permissions;
+import com.restaurante.dsi.model.Users;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+  private static final Logger logger = LoggerFactory.getLogger(UserDetailsImpl.class);
+
+  private static final long serialVersionUID = 1L;
+
+  private Long id;
+
+  private String username;
+
+  private String email;
+
+  @JsonIgnore
+  private String password;
+
+  private Collection<? extends GrantedAuthority> authorities;
+
+  public UserDetailsImpl(Long id, String username, String email, String password,
+      Collection<? extends GrantedAuthority> authorities) {
+    this.id = id;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.authorities = authorities;
+  }
+
+  public static UserDetailsImpl build(Users user) {
+    Set<Permissions> permissions = new HashSet();
+    if (user.getRoles() != null) {
+      permissions = user.getRoles().getPermissions();
+    }
+    List<GrantedAuthority> authorities = permissions.stream()
+        .map(permission -> new SimpleGrantedAuthority(permission.getName()))
+        .collect(Collectors.toList());
+
+    logger.info("Authorities: {}", authorities);
+    return new UserDetailsImpl(
+        user.getId(),
+        user.getUsername(),
+        user.getEmail(),
+        user.getPassword(),
+        authorities);
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorities;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return username;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    UserDetailsImpl user = (UserDetailsImpl) o;
+    return Objects.equals(id, user.id);
+  }
+
+}
