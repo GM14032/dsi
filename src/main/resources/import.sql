dcb202772f53f1a7bde49ee9284229d793e3167f
Jenniffer Granados <waltermarrom777@gmail.com>
2023-06-13 13:09:47 -0600

    add import.sql with products and order details

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -28,5 +28,20 @@ insert into permission_role (role_id, permission_id) values (2, 4);
 insert into users(name,last_name,email,phone,username, password, enable, role_id) values ('Jenniffer','Granados','fiebre.libros@gmail.com','(503) 7714-8798','admin', '$2a$10$eGW9WJuDbUeEfAa060zptusUKtUWZIlra/SBBAG3hfR3Hfn1Y1Age', true, 1);
 insert into users(name,last_name,email,phone,username, password, enable, role_id) values ('Jericho','Barrons','test2@gmail.com','(503) 7489-5478','barrons', '$2a$10$eGW9WJuDbUeEfAa060zptusUKtUWZIlra/SBBAG3hfR3Hfn1Y1Age', true, 1);
 
-INSERT INTO orders ( number_order, category, quantity, description, state, name)
-VALUES ( 1, 'Desayuno', 2, 'Huevos Revueltos con extra queso', 1, 'Huevos Revueltos');
+INSERT INTO order_states (id, name) VALUES (1, 'pendiente');
+INSERT INTO order_states (id, name) VALUES (2, 'preparando');
+INSERT INTO order_states (id, name) VALUES (3, 'completado');
+INSERT INTO order_states (id, name) VALUES (4, 'entregado');
+INSERT INTO order_states (id, name) VALUES (5, 'cancelado');
+
+INSERT INTO products (id, name, price) VALUES (1, 'Huevos Revueltos', 5.00);
+INSERT INTO products (id, name, price) VALUES (2, 'Huevos Rancheros', 5.00);
+INSERT INTO products (id, name, price) VALUES (3, 'Huevos con Chorizo', 5.00);
+INSERT INTO products (id, name, price) VALUES (4, 'Huevos con Jamon', 5.00);
+INSERT INTO products (id, name, price) VALUES (5, 'Huevos con Tocino', 5.00);
+INSERT INTO products (id, name, price) VALUES (6, 'Huevos con Salchicha', 5.00);
+
+INSERT INTO orders (id, number_order, category, quantity, description, state_id) VALUES (1, 1, 'Desayuno', 2, 'Huevos Revueltos con extra queso', 1);
+
+INSERT INTO order_details (id, order_id, product_id, total, quantity) VALUES (1, 1, 1, 10.00, 2);
+INSERT INTO order_details (id, order_id, product_id, total, quantity) VALUES (2, 1, 2, 10.00, 2);
